49b7c945bcff81fd89cd060e80a56abaf909a01b
Zak Johnson <zakj@nox.cx>
2015-09-04 23:59:20 -0700

    Simplify safari.lua

--- hammerspoon/safari.lua
+++ hammerspoon/safari.lua
@@ -1,23 +1,22 @@
 -- Use cmd-n to switch tabs in Safari.
 
-local safariHotkeys = {}
+local hotkeys = hs.hotkey.modal.new()
 
 for i = 1, 9 do
-    local hotkey = hs.hotkey.new({'cmd'}, tostring(i), function()
+    hotkeys:bind({'cmd'}, tostring(i), function()
         hs.applescript.applescript('tell front window of app "Safari" to set current tab to tab ' .. i)
     end)
-    table.insert(safariHotkeys, hotkey)
 end
 
+local eventHandlers = {
+    [hs.application.watcher.activated] = hotkeys.enter,
+    [hs.application.watcher.deactivated] = hotkeys.exit,
+    [hs.application.watcher.terminated] = hotkeys.exit,
+}
+
 hs.application.watcher.new(function(appName, eventType, app)
-    if appName ~= 'Safari' then return end
-    if eventType == hs.application.watcher.activated then
-        for _, key in pairs(safariHotkeys) do
-            key:enable()
-        end
-    elseif eventType == hs.application.watcher.deactivated then
-        for _, key in pairs(safariHotkeys) do
-            key:disable()
-        end
+    handler = eventHandlers[eventType]
+    if handler and appName == 'Safari' then
+        handler(hotkeys)
     end
 end):start()
