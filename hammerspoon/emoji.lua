1e75563e7f2e9b388aba002eee5dc2c927fd2a71
Zak Johnson <me@zakj.net>
2020-09-05 03:51:12 -0700

    Handle out-of-order asyncGet responses in emoji.lua

--- hammerspoon/emoji.lua
+++ hammerspoon/emoji.lua
@@ -2,9 +2,6 @@ local u = require 'util'
 
 exports = {}
 
--- TODO cancel previous call if they're stacking? or just debounce? there's no cancel.
--- maybe global order int, increment per call, only update if it's higher than the last update?
-
 local function imageFromEmoji(emoji)
   local canvas = hs.canvas.new({x = 0, y = 0, w = 100, h = 100}):appendElements({
     frame = {x = 0, y = 5, w = 100, h = 100},
@@ -19,24 +16,26 @@ local function imageFromEmoji(emoji)
   return image
 end
 
+local function resultToChoice(item)
+  local codes = u.map(u.split(item.Code, ' '), function(x) return tonumber(x, 16) end)
+  local emoji = hs.utf8.codepointToUTF8(table.unpack(codes))
+  return {
+    text = string.lower(item.Name),
+    emoji = emoji,
+    image = imageFromEmoji(emoji),
+  }
+end
+
 local function emojiFinder(query, onSuccess)
   local url = 'https://emojifinder.com/*/ajax.php?action=search&query=' .. hs.http.encodeForQuery(query)
   hs.http.asyncGet(url, nil, function(status, body, headers)
     local data = hs.json.decode(body)
     if data.status == 'error' then return end
-
     local results = u.filter(data.results, function(x) return not (x.Code == nil or x.Name == "") end)
-    onSuccess(u.map(results, function(item)
-      local codes = u.map(u.split(item.Code, ' '), function(x) return tonumber(x, 16) end)
-      local emoji = hs.utf8.codepointToUTF8(table.unpack(codes))
-      return {
-        text = string.lower(item.Name),
-        emoji = emoji,
-        image = imageFromEmoji(emoji),
-      }
-    end))
+    onSuccess(u.map(results, resultToChoice))
   end)
 end
+
 
 function exports.chooser()
   local ch = hs.chooser.new(function(item)
@@ -45,10 +44,21 @@ function exports.chooser()
     end
   end)
   ch:placeholderText('Search emoji…')
-  ch:queryChangedCallback(function(q)
-    emojiFinder(q, function(choices) ch:choices(choices) end)
-  end)
   ch:show()
+
+  local lastUpdateId = 0
+  local lastQueryId = 0
+  ch:queryChangedCallback(function(query)
+    lastQueryId = lastQueryId + 1
+    local id = lastQueryId
+
+    emojiFinder(query, function(choices)
+      if id > lastUpdateId then
+        lastUpdateId = id
+        ch:choices(choices)
+      end
+    end)
+  end)
 end
 
 return exports
