438012ca8f6873d0469ce666d137e5919c8721da
Zak Johnson <me@zakj.net>
2021-01-06 13:51:29 -0800

    whitespace only

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,10 @@
-local ctrl         = require 'ctrl'
-local hyper        = require 'hyper'
-local launcher     = require 'launcher'
-local layout       = require 'layout'
-local message      = require 'message'
-local reload       = require 'reload'
-local superClick   = require 'superclick'
+local ctrl = require 'ctrl'
+local hyper = require 'hyper'
+local launcher = require 'launcher'
+local layout = require 'layout'
+local message = require 'message'
+local reload = require 'reload'
+local superClick = require 'superclick'
 
 ctrl:start()
 reload:start()
@@ -16,48 +16,49 @@ hs.window.animationDuration = 0
 local notesPath = '/Users/zakj/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents'
 
 local function withFocusedWindow(...)
-    local varargs = {...}
-    return function()
-        for i, fn in ipairs(varargs) do
-            fn(hs.window.focusedWindow())
-        end
+  local varargs = {...}
+  return function()
+    for i, fn in ipairs(varargs) do
+      fn(hs.window.focusedWindow())
     end
+  end
 end
 
 local function debugWindow(win)
-    local frame = win:frame()
-    local notif = hs.notify.new(nil, {
-        title = win:title(),
-        informativeText = string.format('%s\n(%.0f, %.0f) %.0f⨉%.0f',
-            win:application():title(), frame.x, frame.y, frame.w, frame.h),
-        autoWithdraw = false,
-    })
-    notif:send()
-    hs.timer.doAfter(30, function()
-        notif:withdraw()
-        notif:release()
-    end)
+  local frame = win:frame()
+  local notif = hs.notify.new(nil, {
+    title = win:title(),
+    informativeText = string.format('%s\n(%.0f, %.0f) %.0f⨉%.0f',
+      win:application():title(), frame.x, frame.y, frame.w, frame.h),
+    autoWithdraw = false,
+  })
+  notif:send()
+  hs.timer.doAfter(30, function()
+    notif:withdraw()
+    notif:release()
+  end)
 end
 
 local function toggleDesktopIcons()
-    hs.task.new('/usr/bin/defaults', function(exitCode, stdOut, stdErr)
-        local hidden = exitCode == 0 or stdOut:gsub('%s+', '') == '0'
-            if hidden then
-                os.execute('defaults delete com.apple.finder CreateDesktop')
-            else
-                os.execute('defaults write com.apple.finder CreateDesktop -bool false')
-            end
-            os.execute('killall Finder')
-        end, {'read', 'com.apple.finder', 'CreateDesktop'}):start()
+  hs.task.new('/usr/bin/defaults', function(exitCode, stdOut, stdErr)
+    local hidden = exitCode == 0 or stdOut:gsub('%s+', '') == '0'
+      if hidden then
+        os.execute('defaults delete com.apple.finder CreateDesktop')
+      else
+        os.execute('defaults write com.apple.finder CreateDesktop -bool false')
+      end
+      os.execute('killall Finder')
+    end, {'read', 'com.apple.finder', 'CreateDesktop'}
+  ):start()
 end
 
 local function undock()
-    for path, volume in pairs(hs.fs.volume.allVolumes()) do
-        if not volume.NSURLVolumeIsInternalKey then
-            hs.fs.volume.eject(path)
-        end
+  for path, volume in pairs(hs.fs.volume.allVolumes()) do
+    if not volume.NSURLVolumeIsInternalKey then
+      hs.fs.volume.eject(path)
     end
-    hs.caffeinate.systemSleep()
+  end
+  hs.caffeinate.systemSleep()
 end
 
 local function open(name)
