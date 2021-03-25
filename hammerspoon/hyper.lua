0d48ac366bf58af840e5abdafed93cbebfa26604
Zak Johnson <me@zakj.net>
2021-03-24 20:49:58 -0700

    hs: Briefly delay entering hyperMode
    This allows quick typing of "ds" inside the hyperWindow... mostly useful
    in vim for deleting a surround.

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -4,6 +4,7 @@ local u = require 'util'
 local hyperKeys = {s = true, d = true}
 local hyperMsg = '⌘'
 local releaseWindow = 0.04  -- allowed delay between presses
+local hyperWindow = 0.1 -- hold of hyper keys before activating hyper mode
 
 -- eventtap.new handler returns true to delete an event, false to pass through.
 local SUPPRESS_EVENT = true
@@ -15,21 +16,14 @@ function hyper.new(bindings)
   local self = {}
   local hyperMode = false
   local keyPresses = {}
+  local releaseTimer = hs.timer.new(0, function() end)
+  local hyperTimer = hs.timer.new(0, function() end)
   local modal = hs.hotkey.modal.new()
 
   hs.fnutils.each(bindings, function(binding)
     local key, fn = table.unpack(binding)
     modal:bind({}, key, fn)
   end)
-
-  modal.entered = function()
-    keyPresses = {}
-    message.show(hyperMsg)
-  end
-
-  modal.exited = function()
-    message.hide()
-  end
 
   local function releaseAllKeys()
     if u.len(keyPresses) == 0 then return end
@@ -39,15 +33,16 @@ function hyper.new(bindings)
     self:start()
   end
 
-  local function updateHyperMode()
-    local prev = hyperMode
-    hyperMode = u.len(keyPresses) >= u.len(hyperKeys)
-    if prev == hyperMode then return end
-    if hyperMode then
-      modal:enter()
-    else
-      modal:exit()
-    end
+  modal.entered = function()
+    hyperMode = true
+    keyPresses = {}
+    message.show(hyperMsg)
+  end
+
+  modal.exited = function()
+    hyperMode = false
+    releaseAllKeys()
+    message.hide()
   end
 
   local keyDownTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(e)
@@ -59,8 +54,14 @@ function hyper.new(bindings)
 
     if hyperKeys[chr] and not hasFlags then
       keyPresses[chr] = true
-      hs.timer.doAfter(releaseWindow, releaseAllKeys)
-      updateHyperMode()
+      if u.len(keyPresses) == u.len(hyperKeys) then
+        releaseTimer:stop()
+        if not hyperTimer:running() then
+          hyperTimer = hs.timer.doAfter(hyperWindow, function () modal:enter() end)
+        end
+      else
+        releaseTimer = hs.timer.doAfter(releaseWindow, releaseAllKeys)
+      end
       return SUPPRESS_EVENT
     end
 
@@ -71,12 +72,12 @@ function hyper.new(bindings)
   local keyUpTap = hs.eventtap.new({hs.eventtap.event.types.keyUp}, function(e)
     local chr = e:getCharacters()
     local hasFlags = u.len(e:getFlags()) > 0
+    hyperTimer:stop()
 
     if hyperKeys[chr] and not hasFlags then
       -- We can see a "shift-s" keydown followed by a raw "s" keyup.
       local suppressedKeydown = keyPresses[chr]
-      releaseAllKeys()
-      updateHyperMode()
+      modal:exit()
       return u.fif(suppressedKeydown, SUPPRESS_EVENT, PASSTHROUGH_EVENT)
     end
 
