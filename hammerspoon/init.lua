b98360d753679a36fdec73caf366eec12cb80948
Zak Johnson <me@zakj.net>
2024-07-09 22:26:53 -0700

    hs: Doublemod to hyper
    Instead of entering a hammerspoon-only modal shortcut state on
    double-tapping cmd, use a double-tapped cmd key as a "hyper" modifier
    (all modifiers pressed). This is a slightly nicer UX and has the
    pleasant side effect that the hyper shortcuts are available to
    applications outside of Hammerspoon (like Raycast).
    
    Also cleaned up some now-unused cruft.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,14 +1,11 @@
-local caffeinate = require 'caffeinate'
 local ctrl = require 'ctrl'
-local doublemod = require 'doublemod'
+local hyper = require 'hyper'
 local layout = require 'layout'
 local message = require 'message'
 local reload = require 'reload'
--- local superClick = require 'superclick'
-local u = require 'util'
 
-caffeinate:start()
 ctrl:start()
+hyper:start()
 reload:start()
 
 hs.hints.style = 'vimperator'
@@ -24,106 +21,26 @@ local function withFocusedWindow(...)
   end
 end
 
-local function debugWindow(win)
-  local frame = win:frame()
-  local notif = hs.notify.new(nil, {
-    title = win:title(),
-    informativeText = string.format('%s\n(%.0f, %.0f) %.0f⨉%.0f',
-      win:application():title(), frame.x, frame.y, frame.w, frame.h),
-    autoWithdraw = false,
-  })
-  notif:send()
-  hs.timer.doAfter(30, function()
-    notif:withdraw()
-    notif:release()
-  end)
-end
-
-local function toggleDesktopIcons()
-  hs.task.new('/usr/bin/defaults', function(exitCode, stdOut, stdErr)
-    local hidden = exitCode == 0 or stdOut:gsub('%s+', '') == '0'
-    if hidden then
-      os.execute('defaults delete com.apple.finder CreateDesktop')
-    else
-      os.execute('defaults write com.apple.finder CreateDesktop -bool false')
-    end
-    os.execute('killall Finder')
-  end, { 'read', 'com.apple.finder', 'CreateDesktop' }
-  ):start()
-end
-
-local function undock()
-  for path, volume in pairs(hs.fs.volume.allVolumes()) do
-    if not volume.NSURLVolumeIsInternalKey then
-      hs.fs.volume.eject(path)
-    end
-  end
-  hs.caffeinate.systemSleep()
-end
-
-local function openAndResizeLogseq()
-  hs.application.launchOrFocus('Logseq')
-  hs.timer.waitUntil(
-    function()
-      local app = hs.application.frontmostApplication()
-      return app:name() == 'Logseq' and app:focusedWindow() ~= nil
-    end,
-    function() hs.window.focusedWindow():setSize(850, 1050) end,
-    .1
-  )
-end
-
-local modal = hs.hotkey.modal.new({}, 'f18')
-doublemod.on('cmd', function() modal:enter() end)
-
--- Exit the modal on any keydown (delayed, to make sure modal catches it first).
-local modalTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
-  hs.timer.doAfter(0.1, function() modal:exit() end)
-end)
-modal.entered = function()
-  message.show('⌘')
-  modalTap:start()
-end
-modal.exited = function()
-  message.hide()
-  modalTap:stop()
-end
-
 local function launch(name)
   return function() hs.application.launchOrFocus(name) end
 end
 
-modal
-    -- Utility:
-    :bind({}, 'escape', function() modal:exit() end)
-    :bind({}, ';', hs.caffeinate.lockScreen)
-    :bind({}, "'", function() caffeinate:toggle() end)
-    -- :bind({}, 'u', undock)
-    -- :bind({}, 'x', superClick)
-    -- :bind({}, 'd', toggleDesktopIcons)
+-- TODO: just use raycast with single-character aliases?
+hs.hotkey.bind(hyper.mods, 'f', launch(hs.settings.get('default-browser') or 'Safari'))
+hs.hotkey.bind(hyper.mods, 'l', launch('Slack'))
+hs.hotkey.bind(hyper.mods, 'm', launch('Messages'))
+hs.hotkey.bind(hyper.mods, 'n', launch('Logseq'))
+hs.hotkey.bind(hyper.mods, 't', launch('kitty'))
+hs.hotkey.bind(hyper.mods, 'v', launch('Visual Studio Code'))
 
-    -- Applications:
-    :bind({}, 'f', launch(hs.settings.get('default-browser') or 'Safari'))
-    :bind({}, 'h', launch('Things3'))
-    :bind({}, 'l', launch('Slack'))
-    :bind({}, 'm', launch('Messages'))
-    :bind({}, 'n', launch('Logseq'))
-    :bind({ 'shift' }, 'n', openAndResizeLogseq)
-    :bind({}, 's', launch('Siri'))
-    :bind({}, 't', launch('kitty'))
-    :bind({}, 'v', launch('Visual Studio Code'))
+hs.hotkey.bind(hyper.mods, ';', hs.caffeinate.lockScreen)
+hs.hotkey.bind(hyper.mods, "'", function()
+  hs.execute("open -g raycast://extensions/mooxl/coffee/caffeinateToggle?launchType=background")
+end)
 
-    -- Window layouts:
-    :bind({ 'shift' }, 'k', withFocusedWindow(layout.moveCenter, layout.maximizeV))
-    :bind({}, 'k', withFocusedWindow(layout.moveCenter))
-    :bind({}, 'left', withFocusedWindow(layout.moveTL, layout.maximizeV))
-    :bind({}, 'right', withFocusedWindow(layout.moveTR, layout.maximizeV))
-    :bind({}, '1', withFocusedWindow(layout.sizeQuarter, layout.moveTL))
-    :bind({}, '2', withFocusedWindow(layout.sizeQuarter, layout.moveBL))
-    :bind({}, '3', withFocusedWindow(layout.sizeQuarter, layout.moveTR))
-    :bind({}, '4', withFocusedWindow(layout.sizeQuarter, layout.moveBR))
-    :bind({}, '5', withFocusedWindow(function(w) w:setSize(1320, 870 + 75) end))
-
+hs.hotkey.bind(hyper.mods, 'k', withFocusedWindow(layout.moveCenter))
+hs.hotkey.bind(hyper.mods, 'left', withFocusedWindow(layout.moveTL, layout.maximizeV))
+hs.hotkey.bind(hyper.mods, 'right', withFocusedWindow(layout.moveTR, layout.maximizeV))
 
 -- Make sure garbage collection doesn't break new functionality.
 hs.timer.doAfter(2, collectgarbage)
