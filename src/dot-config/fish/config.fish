e79595460df0dc7540ff1c0057bc493dc8f6e42f
Zak Johnson <me@zakj.net>
2024-08-29 22:31:37 -0700

    fish: clean up and document config

--- src/dot-config/fish/config.fish
+++ src/dot-config/fish/config.fish
@@ -4,30 +4,36 @@ end
 
 if status is-interactive
     fish_config theme choose 'Solarized Dark'
-    set fish_color_valid_path
-    starship init fish | source
+    set fish_color_valid_path    # Reset theme's underlines on paths.
+    starship init fish | source  # Set prompt.
 
-    fish_vi_key_bindings
-    set fish_cursor_insert line
+    fish_hybrid_key_bindings     # vi mode but with emacs-style bindings.
+    set fish_cursor_insert line  # Use cursor to distinguish insert/normal modes.
 
-    set fish_greeting
-    set __fish_ls_command ls -F
+    set fish_greeting            # Shhh.
+    set __fish_ls_command ls -F  # Avoid default ls colors added by fish.
 
+    # Configure some environment variables for other utilities.
     if not set -q EDITOR
         set -l editors (
             path filter -x (which nvim) (which vim) (which vi) | path basename
         )
         set -x EDITOR $editors[1]
     end
-    function EDITOR; echo $EDITOR; end
     set -x LESS gij5MR
     set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep.conf
 
+    # Abbreviations are like bash/zsh aliases, but expand in place.
     bind . self-insert expand-abbr
     abbr --add dot --regex '(\.\./)*\.\.\.' --position anywhere --function rationalise-dot
     abbr --add np --function node-package-managers
     abbr --add psg pgrep -lf
     abbr --add vi --function EDITOR
+
+    # Allow `vi` abbr to respond to changes to $EDITOR.
+    function EDITOR
+        echo $EDITOR
+    end
 
     # Normalize fancy grep tools.
     function g
